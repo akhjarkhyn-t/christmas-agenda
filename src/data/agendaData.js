044b4236e4ee1d582caea7b25745b72e8d7148aa
akhjarkhyn-t <akhjarkhyn.tilyeu@gmail.com>
2025-12-19 07:47:19 +0700

    update

--- src/data/agendaData.js
+++ src/data/agendaData.js
@@ -1,59 +1,59 @@
 export const agendaData = [
   {
-    time: "19:40",
+    time: "17:40",
     title: "Хүлээн авалт",
     description: "Зочдыг хүлээн авах, бүртгэл",
     image: "https://media1.giphy.com/media/v1.Y2lkPTZjMDliOTUydm9xcDU0cHZuazdxaWJ0c3B1bnVrNzF2OTVjcnowYnVkMmx6NnFmbSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/um2kBnfo55iW4ZH1Fa/giphy.gif",
   },
   {
-    time: "20:00",
+    time: "18:00",
     title: "Нээлт",
     description: "Арга хэмжээний албан ёсны нээлт",
   },
   {
-    time: "20:10",
+    time: "18:10",
     title: "Тоглоом: Би хэн бэ?",
     description:
       "Тухайн хүний тухай факт хэлнэ. Таасан хүнд +1 оноо.",
   },
   {
-    time: "20:25",
+    time: "18:25",
     title: "Артист 1",
     description: "Нээлтийн дараах артистын тоглолт",
     image: "https://scontent.fuln8-1.fna.fbcdn.net/v/t39.30808-6/493100421_2457644198043221_8351515497185953709_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=833d8c&_nc_ohc=JcZmtnlGlfQQ7kNvwFhKvh4&_nc_oc=Admfa9sD6WQE3tba3gb7-6f83SjOo_FzgboDQ7rE3dnq0v5F-gfloJDzF9jmdNj-C7w&_nc_zt=23&_nc_ht=scontent.fuln8-1.fna&_nc_gid=hp3G53pNig7vVgSlewA4eQ&oh=00_AfkMqSwpZDOspMtDgE0nQ5-Ws_NYFl-iyrfrGMrSq1Jt0Q&oe=694A5AC5",
     blur: true,
   },
   {
-    time: "20:35",
+    time: "18:35",
     title: "Хооллох",
     description: "Уух, хооллох завсарлага",
   },
   {
-    time: "21:00",
+    time: "19:00",
     title: "Тоглоом: Санал хураалт",
     description:
       "Ажилтай холбоотой асуулт асууна. Санал хураалт явагдана. Хамгийн олон санал авсан хүнд +1 оноо.",
   },
   {
-    time: "21:15",
+    time: "19:15",
     title: "Хооллох",
     description: "Уух, хооллох завсарлага",
   },
   {
-    time: "21:45",
+    time: "19:45",
     title: "Артист 2",
     description: "Хоолны дараах артистын тоглолт",
     image: "https://scontent.fuln8-1.fna.fbcdn.net/v/t39.30808-6/493113636_2457643791376595_1105025929788019431_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=107&ccb=1-7&_nc_sid=833d8c&_nc_ohc=MhT0RDT5mHwQ7kNvwEsSVOd&_nc_oc=Adl8txi07VY7aCygL8oZ8oH5AlK8mF8YhsFplVXDm6K_brlvQveDLfymykXEhlsOzZk&_nc_zt=23&_nc_ht=scontent.fuln8-1.fna&_nc_gid=lFbSXP1vMi0TgQvmyoNC2A&oh=00_AfnSEs5sCwPiZTV4gza6neO7q35sgNlzuHzG9-osqZvCCw&oe=694A2B04",
     blur: true,
   },
   {
-    time: "21:55",
+    time: "19:55",
     title: "Тоглоом: Secret Santa таах",
     description:
       "Нууц сантааны тухай ярьж, хэн бэлэг өгч байгааг таана. Таасан хүнд +2, дөхүүлсэн +1.",
   },
   {
-    time: "22:30",
+    time: "20:30",
     title: "Secret Santa reveal",
     description:
       "Оноогоор дуудаж бэлэг өгөх. Сэтгэгдэл хэлэх. Бэлэг өгсөн хүн гарч ирэх. Зураг авах.",
@@ -61,24 +61,24 @@ export const agendaData = [
     blur: false,
   },
   {
-    time: "22:45",
+    time: "20:45",
     title: "Сугалаа",
     description: "Сугалаа явуулах",
   },
   {
-    time: "22:55",
+    time: "20:55",
     title: "Шагнал",
     description: "Ялагчдыг зарлаж, шагнал гардуулах",
   },
   {
-    time: "23:05",
+    time: "20:05",
     title: "Артист 3",
     description: "Бүжиг / free time үеийн артистын тоглолт",
     image: "https://scontent.fuln8-1.fna.fbcdn.net/v/t39.30808-6/492500907_2457643961376578_8180048460448347954_n.jpg?stp=c0.225.1366.1366a_cp6_dst-jpg_s206x206_tt6&_nc_cat=110&ccb=1-7&_nc_sid=50ad20&_nc_ohc=XImhqINu0wAQ7kNvwG512WF&_nc_oc=AdnImlPF_vIlUiSYwx53kn1vZias-zfTV8PwckoaphzLQpvbCv-peDZWGwdAKKLrfHg&_nc_zt=23&_nc_ht=scontent.fuln8-1.fna&_nc_gid=Di-Y0aF5EUcgsPHWPXSsYw&oh=00_AfkJ-D-Nfi9yQXI8SBdBz9I47zYfaWlHpjbzCsrVmHMLNg&oe=694A45DC",
     blur: true,
   },
   {
-    time: "23:15",
+    time: "20:15",
     title: "Free time",
     description: "Чөлөөт цаг, бүжиг, зураг авалт",
     image: "https://media.tenor.com/RdYowW9KtNMAAAAM/dancing-happy-dance.gif",
